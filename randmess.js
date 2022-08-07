729bb9efa8007d882a9728be16b7351579e3f23d
Blatherdicer <1078975+blatherdicer@users.noreply.github.com>
2022-08-07 20:41:20 +1200

    Working version

--- randmess.js
+++ randmess.js
@@ -1,4 +1,22 @@
-function clickHandler() {
-  let outputDiv = document.getElementById('message-output');
-  outputDiv.innerHTML="<p>Hello</p>";
+
+const times = [ 'In five minutes','Next week','In a year or two','In 2073','In two millennia'];
+const causes = ['a massive meteorite will impact the planet','a mutated virus will run rampant amongst the world\'s population','an AI that becomes sentient','a climate tipping point','a breakdown of society','a supervolcano eruption','a military robot uprising','a nano-technology chain reaction','runaway global warming','an unforseen climate reversal will trigger an ice age and'];
+const outcomes = ['thermal overload in the atmosphere','sterility of all males in the population','the world and everything in it to dissolve into grey goo','massive firestorms and runaway temperature increases','breakdown of the food chain and starvation of the population','zombie mutants to eat the population','the atmosphere to vaporise and the seas to boil away','loss of all food sources and societal breakdown'];
+
+const generateMessage = () => {
+  let message = '';
+  let randomTime = times[Math.floor(Math.random()*times.length)];
+  let randomCause = causes[Math.floor(Math.random()*causes.length)];
+  let randomOutcome = outcomes[Math.floor(Math.random()*outcomes.length)];
+  return randomTime+', '+randomCause+' will cause '+randomOutcome+'.';
 }
+
+const clickHandler = () => {
+  let outputDiv = document.getElementById('message-output');
+  outputDiv.style.visibility = "visible";
+  outputDiv.innerHTML=`<p>${generateMessage()}</p>`;
+  let buttonText = document.querySelector('button');
+  buttonText.innerHTML="... or try your luck with another parallel universe?";
+}
+
+console.log(generateMessage());
