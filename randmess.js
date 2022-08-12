51045cf2bec4df69a1cabb0e38fb4198bf2d413f
Blatherdicer <1078975+blatherdicer@users.noreply.github.com>
2022-08-12 16:30:12 +1200

    Correct function syntax.

--- randmess.js
+++ randmess.js
@@ -2,14 +2,14 @@ const times = [ 'In five minutes','Next week','In a year or two','In 2073','In t
 const causes = ['a massive meteorite will impact the planet','a mutated virus will run rampant amongst the world\'s population','an AI that becomes sentient','a climate tipping point','a breakdown of society','a supervolcano eruption','a military robot uprising','a nano-technology chain reaction','runaway global warming','an unforseen climate reversal will trigger an ice age and'];
 const outcomes = ['thermal overload in the atmosphere','sterility of all males in the population','the world and everything in it to dissolve into grey goo','massive firestorms and runaway temperature increases','breakdown of the food chain and starvation of the population','zombie mutants to eat the population','the atmosphere to vaporise and the seas to boil away','loss of all food sources and societal breakdown'];
 
-const generateMessage = () => {
+const generateMessage = function () {
   const randPhrase = (phrases) => {
     return phrases[Math.floor(Math.random()*phrases.length)]
   }
   return `${randPhrase(times)}, ${randPhrase(causes)} will cause ${randPhrase(outcomes)}`;
 }
 
-const clickHandler = () => {
+const clickHandler = function () {
   let outputDiv = document.getElementById('message-output');
   outputDiv.style.visibility = "visible";
   outputDiv.innerHTML=`<p>${generateMessage()}</p>`;
@@ -17,4 +17,4 @@ const clickHandler = () => {
   buttonText.innerHTML="... or try your luck with another parallel universe?";
 }
 
-console.log(generateMessage());
+console.log(generateMessage()); 
